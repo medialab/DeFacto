e6e37245dc5111beffa543b632554b20d6587024
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-01-23 11:11:14 +0100

    streamline text preprocessing and add word freq count

--- nlp/prep_data.py
+++ nlp/prep_data.py
@@ -1,26 +1,43 @@
 import casanova
 from nltk.corpus import stopwords
+from collections import defaultdict
 
 stoplist = stopwords.words("french")
 ADDITIONAL_STOPWORDS = ["plus", "chaque", "tout", "tous", "toutes", "toute", "leur", "leurs", "comme", "afin", "pour"]
 PUNCTUATION = ["»", "«", "?", "!", ".", "%", ",", ".", "(", ")", ":", "’", "&", ";" '"']
 
+
 def clean_propositions(file, column)-> list[list[str]]:
+
     stoplist.extend(ADDITIONAL_STOPWORDS)
+
+    frequency = defaultdict(int)
+
     with open(file) as f:
         reader = casanova.reader(f)
-        cleaned_bags_of_words = []
-        for cell in reader.cells(column):
-            proposition = cell.lower()
-            decomposed_proposition = []
-            for char in proposition:
-                if char == "/" or char == "'" or char == "’": char = " "
-                elif char in PUNCTUATION: char = ""
-                decomposed_proposition.append(char)
-            proposition = "".join(decomposed_proposition)
-            bag_of_words = [word for word in proposition.split()]
-            bag_of_words = [w for w in bag_of_words if w not in stoplist]
-            if bag_of_words[0] == "faut":
-                bag_of_words = bag_of_words[1:]
-            cleaned_bags_of_words.append(bag_of_words)
-        return cleaned_bags_of_words
+
+        # remove stopwords and punctuation
+        texts = [
+            [word for word in remove_punctuation(document).lower().split() if word not in stoplist]
+            for document in reader.cells(column=column)
+        ]
+
+        # Count word frequencies
+        for text in texts:
+            for word in text:
+                print(word)
+                frequency[word] += 1
+
+        # Only keep words that appear more than once
+        processed_corpus = [[token for token in text if frequency[token] > 1] for text in texts]
+
+        return processed_corpus
+
+
+def remove_punctuation(document):
+    decomposed_string = []
+    for char in document:
+        if char == "/" or char == "'" or char == "’": char = " "
+        elif char in PUNCTUATION: char = ""
+        decomposed_string.append(char)
+    return "".join(decomposed_string)
